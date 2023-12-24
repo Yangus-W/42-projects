b88ffe22770571e72a5afae36b1d8a78296d2acc
Yang Wang <yawang@bess-f1r3s3.clusters.42paris.fr>
2023-12-24 19:36:58 +0100

    put children functions into differents files

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -5,61 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yawang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/13 20:49:05 by yawang            #+#    #+#             */
-/*   Updated: 2023/12/19 21:28:50 by yawang           ###   ########.fr       */
+/*   Created: 2023/12/24 17:18:51 by yawang            #+#    #+#             */
+/*   Updated: 2023/12/24 19:26:48 by yawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdarg.h>
-
-int	print_char(int c)
-{
-	return (write(1, &c, 1));
-}
-
-int	print_str(char *str)
-{
-	int	count;
-
-	count = 0;
-	while (*str)
-	{
-		print_char((int)*str);
-		++count;
-		++str;
-	}
-	return (count);
-}
-
-int	print_digit(long n, int base)
-{
-	int		count;
-	char	*symbols;
-
-	symbols = "0123456789abcdef";
-	if (n < 0)
-	{
-		write(1, "-", 1);
-		return (print_digit(-n, base) + 1);
-	}
-	else if (n < base)
-	{
-		return (print_char(symbols[n]));
-	}
-	else
-	{
-		count = print_digit(n / base, base);
-		return (count + print_digit(n % base, base));
-	}
-}
-
-int	print_ptr(void *ptr)
-{
-	write(1, "0x", 2);
-	return (print_digit((long)ptr, 16) + 2);
-}
+#include "ft_printf.h"
 
 int	print_format(char specifier, va_list ap)
 {
@@ -73,11 +24,15 @@ int	print_format(char specifier, va_list ap)
 	else if (specifier == 'p')
 		count += print_ptr(va_arg(ap, void *));
 	else if (specifier == 'd' || specifier == 'i')
-		count += print_digit((long)(va_arg(ap, int)), 10);
-	else if (specifier == 'u')
-		count += print_digit((unsigned long)(va_arg(ap, int)), 10);
+		count += print_digit((long)(va_arg(ap, int)), 10, 1);
+	// else if (specifier == 'u')
+	// 	count += print_unsigned_int((unsigned long)(va_arg(ap, int)), 10);
 	else if (specifier == 'x')
-		count += print_digit((long)(va_arg(ap, unsigned int)), 16);
+		count += print_digit((long)(va_arg(ap, unsigned int)), 16, 1);
+	else if (specifier == 'X')
+		count += print_digit((long)(va_arg(ap, unsigned int)), 16, 2);
+	else if (specifier == '%')
+		count += print_char('%');
 	else
 		count += write(1, &specifier, 1);
 	return (count);
@@ -132,6 +87,21 @@ int	main(void)
 	count = ft_printf("%u\n", -109);
 	ft_printf("The chars written are %d\n", count);
 	count = printf("%u\n", -109);
+	printf("The chars written are %d\n", count);
+	
+	count = ft_printf("%%\n", '%');
+	ft_printf("The chars written are %d\n", count);
+	count = printf("%c\n", '%');
+	printf("The chars written are %d\n", count);
+	
+	count = ft_printf("%x\n", 111);
+	ft_printf("The chars written are %d\n", count);
+	count = printf("%x\n", 111);
+	printf("The chars written are %d\n", count);
+	
+	count = ft_printf("%X\n", 111);
+	ft_printf("The chars written are %d\n", count);
+	count = printf("%X\n", 111);
 	printf("The chars written are %d\n", count);
 }
 // write returns the number of bytes upon successful completion
