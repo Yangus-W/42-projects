4dd79d555411f8b2d22985b6170f30ed2c0f91cf
Yang Wang <yawang@paul-f3Ar4s6.clusters.42paris.fr>
2024-04-05 19:17:04 +0200

    try

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,17 @@
 /*   By: yawang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 19:53:43 by yawang            #+#    #+#             */
-/*   Updated: 2023/12/25 19:53:45 by yawang           ###   ########.fr       */
+/*   Updated: 2024/04/05 19:16:09 by yawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef	GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+
+#endif
